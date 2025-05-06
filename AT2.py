7cac156c7238cd622aa8b1515866c2939f79be71
theTiagoSales <tiagosales0020@gmail.com>
2025-05-06 16:52:43 -0300

    Create AT2.py

---
+++ AT2.py
@@ -0,0 +1,23 @@
+# contagem
+
+for i in range(0, 31, 2):
+    if i == 10 or i == 20 or i == 30:
+        continue
+    print(i)
+
+# media com condicionais
+
+n1 = float(input('insira sua primeira nota de 0 a 10: '))
+while n1 < 0 or n1 > 10:
+    print('insira uma nota válida')
+    n1 = float(input('insira sua primeira nota de 0 a 10: '))
+n2 = float(input('insira sua segunda nota de 0 a 10: '))
+while n2 < 0 or n2 > 10:
+    print('insira uma nota válida')
+    n2 = float(input('insira sua segunda nota de 0 a 10: '))
+média = (n1 + n2) / 2
+print('sua média é', média)
+if média >= 6:
+   print('você foi aprovado!')
+else:
+   print('aluno reprovado.')
