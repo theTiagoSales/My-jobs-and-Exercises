a73055e297007cf1e063dce68d9a099c9818d9b8
theTiagoSales <tiagosales0020@gmail.com>
2025-05-09 13:44:24 -0300

    AT3(lista).py

---
+++ AT3(lista).py
@@ -0,0 +1,55 @@
+# criando lista
+
+lista = ['cachorro', 'gato', 'papagaio']
+
+#adicionando ao final da lista
+
+lista.append('hamster')
+
+# adicionando na primeira posição da lista
+
+lista.insert(1,'coelho')
+
+# removendo o último animal da lista
+
+lista.pop()
+
+# removendo o animal da posição 2
+
+lista.pop(2)
+
+# alterando o primeiro elemento da lista
+
+lista[0] = 'leão'
+
+print(lista)
+
+# criando a matriz
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6], 
+    [7, 8, 9]
+]
+
+# selecionando o número 5
+
+print(matriz[1][1])
+
+#criando lista do problema 3
+
+lista2 = [10, 20, [30, 40], 'hello']
+
+# verificando o comprimento
+
+len(lista2)
+
+# criando lista do problema 4
+
+lista3 = ['morango', 'abacaxi', 'goiaba', 'jambo', 'banana']
+
+# selecionando o primeiro e terceiro elemento da lista
+
+print(lista3[0])
+
+print(lista3[2])
